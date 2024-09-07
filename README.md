56a38e0c4b4c8aed9fcec32d3dd4bb082c188126
Ethan Morton <ethieq909@gmail.com>
2024-09-07 02:53:37 -0400

    updated readme and commands

--- README.md
+++ README.md
@@ -6,4 +6,7 @@ See https://www.labouseur.com/courses/os/ for details.
 It was originally developed by Alan and then enhanced by Bob Nisco and Rebecca Murphy over the years.
 ___
 # Assembler/Text Editor
+I made this 6502 assembler/text-editor for Org and Arch in Spring 2024.
+It should use the same instruction set as OS.
+
 https://github.com/eclipse-909/Assembler-6502
--- commands.sh
+++ commands.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-function run() {
+# Compile TypeScript and open the OS in FireFox or your default browser.
+run() {
   tsc
-  firefox ./index.html || start ./index.html || xdg-open ./index.html
+  # Brave is my default browser but it doesn't allow precise location, so I run FireFox instead.
+  # If FireFox isn't in your $PATH, start/xdg-open/open will open your default browser on Windows/Linux/Mac respectively.
+  firefox ./index.html || start ./index.html || xdg-open ./index.html || open ./index.html
 }
 
-function push() {
+push() {
 	if [ -z "$1" ]; then
 		echo "Error: Commit message is required"
 		return
